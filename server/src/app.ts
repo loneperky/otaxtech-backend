b653d7cd94526ce8c47800425f5702b6cab867f4
loneperky <loneperky@gmail.com>
2025-06-14 01:40:10 -0700

    Did some modification in linking the client side to the server side

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"], // Adjust to match frontend URLs
+    origin: ["http://localhost:5173", "http://localhost:5174","https://portfolio-swart-psi-80.vercel.app/"], // Adjust to match frontend URLs
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // Include this if you're dealing with cookies or auth headers
--- server/src/models/connectDB.ts
+++ server/src/models/connectDB.ts
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const MONGO_URI: any = process.env.MONGO_URI
+const MONGO_URI = process.env.MONGO_URI as string;
+if (!MONGO_URI) { 
+  console.error("MONGO_URI is not defined in the environment variables.");
+  process.exit(1);
+}
 
 const connectDB = async () => {
   try {
